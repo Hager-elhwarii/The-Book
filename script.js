8bf453e1548d873cac79304671f7eb4d3aff3d41
hager-dot <hagerelhwarii6633@gmail.com>
2024-05-08 00:12:49 +0300

    :sparkles: Add js comments

---
+++ script.js
@@ -0,0 +1,53 @@
+/// variable is a label to data.
+// array is a way to store several data.
+// String is not Immutabil, meanwhile Array is mutabil
+// ? var x = "gii"
+// ? x[0] = 'F' this will not work
+// & var x = ['gi','roo','gkk']
+// & x[0] = 'lle' this will work
+// name.length to count letters in a string.
+// name[0] to get the first letter of a string.
+// name.length-1 means that it will get the last letter.
+// push add to the end of the array, unshift => add to the start of an array
+// pop remove from the end of an array, shift => remove from the start ;
+// functions let us write reuseable block of code.
+// var is global scope , and function scope.
+// local variable overide global variable.
+// function with no return statment return undefined.
+// (else if) is a way of chaining if conditions together.
+// if statment for complex conditions and for multiple variables with logical operators.
+// switch statment for simple conditons with single variable.
+// defualt value at the end of the switch statement if all the cases wasn't true.
+// we access object with dot notations and bracket notation.
+// we can modify or add new proprties in object with dot notation.
+// we can remove property from object using the delete key word + myObj.name.
+// check for proprties with obj.hasOwnProperty(Key) return a boolean true or false.
+// while that run spesific code when the condition is true and stops if it's false.
+// for loop for iteration consists of initialztion and conditions then the expresstion.
+// we use for if we know number of iteration , while when condition is true and stop when it's false.
+// do while makes sure that the code is excuted at least once before checking the condition.
+// ? do {
+// ?    Array.push(5)
+// ? } while (i < 10)
+// meanwhile while check the condition first before excuting the code.
+// & ******************************** array methods *********************************
+// forEach => have no return, always return undefined.excute function on each elemnent.
+// map => creates new array after excuting some logic on each elemnet, have a return.
+// filter => creates a shallow copy of an array, filter element based on their conditon.
+// reduce => return single value from an array, EX array.reduce(callback, initialValue);
+// slice => return a shallow copy of an array, which has the start and end (end not included). does not change the original array.
+// splice => modify the original array, with removing, replacing and adding, toSpliced to not mutate the orginal array.
+// sort => sort element of an array (asending) 1 , 10, modify the orginal array.
+// concat => merge to arrays in one array. creates new array, does not modify the orginal array.
+// fill => modify the original array and return it, with (value,start,end).
+// includes => return boolean ture or false.
+// every => return boolean ture if all elements have the same condition.
+// some => return boolean ture  if some elements have the  condition.
+// join => creates anew string of an array by conctanating strings.
+// reverse => reverse an array, modify the orginal array array.
+// indexOf => return the index of a elment of an array, arr.indexOf('car') => 1.
+// LastindexOf => return the index of a elment of an array, but start backwords.
+// find => The find() method of Array instances returns the first element in the provided array that
+// satisfies the provided testing function. If no values satisfy the testing function, undefined is returned.
+// & ********************** ---------------------------------- **********************
+// scoope => where variables are defined and accessed.
